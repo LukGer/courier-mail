60b7c1f24b6371c310b3cab45acb5721016ce1b1
Lukas Gerhold <lukger1999@gmail.com>
2025-03-11 18:40:38 +0100

    courier-mail

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import "../app.css";
+
+  import AppSidebar from "@/components/AppSidebar.svelte";
+  import * as Sidebar from "@/components/ui/sidebar";
+
+  let { children } = $props();
 </script>
 
-{@render children()}
+<Sidebar.Provider>
+  <AppSidebar />
+  <main class="m-2 rounded-lg bg-white shadow-md flex-1">
+    {@render children()}
+  </main>
+</Sidebar.Provider>
