69aa436e53a0c3a222191433fa109eecfbfbb7d3
Lukas Gerhold <lukger1999@gmail.com>
2025-03-20 07:54:01 +0100

    something

--- apps/client/src/app.tsx
+++ apps/client/src/app.tsx
@@ -1,10 +1,27 @@
 import { useAuth, useClerk } from "@clerk/clerk-react";
 import { RouterProvider } from "@tanstack/react-router";
+import { migrate } from "drizzle-orm/sqlite-proxy/migrator";
+import { useEffect } from "react";
+import { db } from "./db";
 import router from "./router";
 
 const App = () => {
   const { isSignedIn, isLoaded } = useAuth();
   const auth = useClerk();
+
+  useEffect(() => {
+    const migrateDatabase = async () => {
+      await migrate(
+        db,
+        async () => {
+          console.log("Seas");
+        },
+        { migrationsFolder: "../../migrations" }
+      );
+    };
+
+    migrateDatabase();
+  }, [auth]);
 
   if (!isLoaded) {
     return <div>Loading...</div>;
